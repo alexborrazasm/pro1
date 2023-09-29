694384e24ce9b8ca0706b12153462e4c4c8e11a5
alexborrazasm <alexborrazasm@gmail.com>
2023-09-30 01:30:04 +0200

    boletin_1 update

---
+++ boletin_1/15.c
@@ -0,0 +1,10 @@
+/* Un camión transporta T kilogramos de baldosas; se sabe que cada baldosa pesa B kilogramos. Las baldosas se van a utilizar para recubrir una superficie 
+rectangular. Plantee e implemente un programa en Lenguaje C que indique el número de baldosas que habría en un lado del mayor cuadrado que se pueda recubrir 
+con ésas baldosas, sin romperlas. */
+
+#include<stdio.h>
+
+main() {
+    
+    return 0;
+}
